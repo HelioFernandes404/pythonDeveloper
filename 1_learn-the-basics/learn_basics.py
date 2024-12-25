7d03c0c4f2bb428f11d175073d1f11fb00cc8c53
HelioFernandes404 <heliojobs25@gmail.com>
2024-12-25 20:21:30 -0300

    estrutura inicial

---
+++ 1_learn-the-basics/learn_basics.py
@@ -0,0 +1,17 @@
+# Basic Syntax
+
+# Variables and Data Types
+
+# Coditionals
+
+# Loops
+
+# Type Casting
+
+# Exceptions
+
+# Functions, builtin functions
+
+# List, Tuple, Set,
+
+# Dictionaries
---
+++ 2_data-structures-and-algorithms/data_structures_algorithms.py
@@ -0,0 +1,11 @@
+# Arrays and linked Lists
+
+# Hash Tables
+
+# Heaps, stacks, and queues
+
+# binary search trees
+
+# Recursion
+
+# sorting algorithms
