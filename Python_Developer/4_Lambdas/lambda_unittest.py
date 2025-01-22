d0df0e1f48c81f203b29784773b3de78aa03cf3e
HelioFernandes404 <heliojobs25@gmail.com>
2025-01-22 17:12:19 -0300

    chore: new config and remove

---
+++ Python_Developer/4_Lambdas/lambda_unittest.py
@@ -0,0 +1,7 @@
+import unittest
+
+addtwo = lambda x: x + 2
+
+class LambdaTest(unittest.TestCase):
+    def test_addtwo(self):
+        def test_add_two
--- Python_Developer/4_Lambdas/real_py_lambda.py
+++ Python_Developer/4_Lambdas/real_py_lambda.py
@@ -96,19 +96,79 @@ div_zero(2)
 (lambda **kwargs: sum(kwargs.values()))(one=1, two=2, three=3)  # Output: 6
 (lambda x, *, y=0, z=0: x + y + z)(1, y=2, z=3)  # Output: 6
 
+
 # Decorators
+def some_decorator(f):
+    def wraps(*args, **kwargs):
+        print("Calling function")
+        return f(*args, **kwargs)
+
+    return wraps
+
+
+@some_decorator
+def decorated_function(x):
+    return x + 1
 
 
 # Closure
+def outer_func(x):
+    def inner_func(y):
+        return x + y
+
+    return inner_func
+
+
+for i in range(3):
+    closure = outer_func(i)
+    print(closure(i + 1))  # Output: 1, 3, 5
+
+
 # Evaluation Time
+def wrap(x):
+    print("wrap()")
+    return x
+
+
+numbers = ["one", "two", "three"]
+
+funcs = []
+
+for n in numbers:
+    funcs.append(lambda: wrap(n))
+
+for f in funcs:
+    print(f())
+# Output: three, three, three
+
+
 # Testing Lambdas
+
+
 # Lambda Expression Abuses
 # Raising an Exception
 # Cryptic Style
 # Python Classes
 # Appropriate Uses of Lambda Expressions
 # Classic Functional Constructs
+list(map(lambda x: x.upper(), ["cat", "dog", "cow"]))  #
+
+list(filter(lambda x: "o" in x, ["cat", "dog", "cow"]))  # Output: ['dog', 'cow']
+
+from functools import reduce
+
+# reduce in cat dog cow
+reduce(
+    lambda acc, x: f"{acc} | {x}", ["cat", "dog", "cow"]
+)  # Output: 'cat | dog | cow'
+
+
 # Key Functions
+ids = ["id1", "id2", "id30", "id3", "id22", "id100"]
+
+print(sorted(ids))  # Output: ['id1', 'id100', 'id2', 'id22', 'id3', 'id30']
+
+
 # UI Frameworks
 # Python Interpreter
 # timeit
