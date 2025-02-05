790e39144ff5d4fdd9f1e8cec9811e37333e5545
heliofernandes404 <heliodevhub@gmail.com>
2025-02-05 09:29:08 -0300

    feat: new name:

---
+++ python_developer/7_oop/oop_simples.py
@@ -0,0 +1,17 @@
+class Dog:
+    pass
+
+
+# Atributos
+class Dog:
+
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# Instanciando a classe
+miles = Dog("Miles", 4)
+buddy = Dog("Buddy", 9)
